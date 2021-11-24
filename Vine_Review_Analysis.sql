84ffc66b748de4c8b8dc931611a8123dc0d40670
mrodenberg9055 <88349448+mrodenberg9055@users.noreply.github.com>
2021-11-23 23:19:30 -0600

    Update Vine_Review_Analysis.sql

--- Vine_Review_Analysis.sql
+++ Vine_Review_Analysis.sql
@@ -52,20 +52,34 @@ AS
 			THEN 1
  		ELSE 0
  	END AS paid
+ 	, CASE 
+		WHEN vine = 'Y'
+ 		AND star_rating = 5
+			THEN 1
+ 		ELSE 0
+ 	END AS paid_five_star
 	, CASE
  		WHEN vine = 'N'
 			THEN 1
 		ELSE 0
 	END AS unpaid
+ 	, CASE
+ 		WHEN vine = 'N'
+ 		AND star_rating = 5
+			THEN 1
+		ELSE 0
+	END AS unpaid_five_star
 FROM vine_table
 )
 
-SELECT COUNT(review_id) AS total_reviews
-	, SUM(five_star_rating) AS five_star_rating
-	, SUM(COALESCE(paid, 0)) AS total_paid
-	, SUM(COALESCE(paid, 0)) / COUNT(review_id)::FLOAT AS paid_percent
-	, SUM(COALESCE(unpaid, 0)) AS total_unpaid
-	, SUM(COALESCE(unpaid, 0)) / COUNT(review_id)::FLOAT  AS unpaid_percent
-FROM reviews
 
+SELECT COUNT(review_id) AS total_reviews
+	, SUM(five_star_rating) AS total_five_star_rating
+	, SUM(COALESCE(paid, 0)) AS total_paid
+	, SUM(COALESCE(paid_five_star, 0)) paid_five_star
+	, SUM(COALESCE(paid_five_star, 0)) / SUM(COALESCE(paid, 0))::FLOAT AS paid_percent
+	, SUM(COALESCE(unpaid_five_star, 0)) AS unpaid_five_star
+	, SUM(COALESCE(unpaid, 0)) AS total_unpaid
+	, SUM(COALESCE(unpaid_five_star, 0)) / SUM(COALESCE(unpaid, 0))::FLOAT  AS unpaid_percent
+FROM reviews
 
